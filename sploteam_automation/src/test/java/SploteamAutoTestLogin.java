c7ad37e4a98273670e45e49112c4f9adccb7b3cc
Tatyana Sinyakova <tatyanasinyakova@Tatyanas-MacBook-Air.local>
2024-01-14 16:53:17 +0100

    first commit

---
+++ sploteam_automation/src/test/java/SploteamAutoTestLogin.java
@@ -0,0 +1,40 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+import static java.lang.Thread.sleep;
+
+
+public class SploteamAutoTestLogin{
+    public static final String url = "https://frontend.beta-spltm.ru";
+    WebDriver driver = new ChromeDriver();
+
+
+
+    @Before
+    public void setup(){
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        ((HasAuthentication) driver).register(UsernameAndPassword.of("sploteam", "Esthotim2#"));
+        driver.get(url);
+    }
+
+
+    @After
+    public void close() {
+        driver.close();
+    }
+
+
+
+    @Test
+    public void clickOnLoginButton(){
+        driver.findElement(By.className(SIGNIN_BUTTON_CLASS)).click();
+
+    }
+
+    public static final String SIGNIN_BUTTON_CLASS = "header__signIn";
+}
